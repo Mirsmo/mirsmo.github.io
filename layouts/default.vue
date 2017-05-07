44d4ff61b2bc971fea2bb4cf16bafc713ff64e10
Levi Beach <levimbeach@gmail.com>
2017-05-07 18:28:35 -0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Rosario');
+  body {
+    font-family: 'Rosario';
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="container">
+    <h1 class="title">Mirsmo</h1>
+    <h2 class="subtitle">Coming soon</h2>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  $ikb: #002EA7;
+  .container {
+    padding: 4vh;
+    left: 50%;
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+  h1 {
+    border: .65vh solid $ikb;
+    color: $ikb;
+    font-size: 8vh;
+    font-weight: normal;
+    text-transform: uppercase;
+    letter-spacing: .25em;
+    padding-left: .25em;
+  }
+  h2 {
+    // color: lighten(black,75%);
+    font-size: 2vh;
+    text-transform: uppercase;
+    letter-spacing: .25em;
+    padding-left: .25em;
+  }
+</style>
